246b853e1339982f1e5d04f601f9369569225312
DESKTOP-SLP526E\moksu <qotnahr@naver.com>
2022-08-25 18:44:07 +0900

    remap

--- remap.py
+++ remap.py
@@ -10,9 +10,9 @@ if src is None:
     sys.exit()
 
 h, w = src.shape[:2]
-
 map2, map1 = np.indices((h, w), dtype=np.float32)
-map2 = map2 + 10 * np.sin(map1 / 32)
+print(map2, map1)
+map2 = map2 + 10 * np.sin(map1 / 50)
 
 dst = cv2.remap(src, map1, map2, cv2.INTER_CUBIC, borderMode=cv2.BORDER_DEFAULT)
 
